bdb4b66baf35998fff1bbac4ce9bf4faace77d2a
kat b <secretlydiamond@gmail.com>
2024-10-11 18:00:49 +0100

    Update CombatConsole.cs

--- CombatConsole/CombatConsole.cs
+++ CombatConsole/CombatConsole.cs
@@ -6,10 +6,12 @@ internal class CombatConsole
 {
    static void Main(string[] args)
    {
+      Dictionary<>
       //Rng.ReplaceSeed(new Random().Next());
 
-      var combatants = FightBuilder.CreateScenario2V2();
+      var combatants = FightBuilder.CreateScenario1V1();
       var combat = new Combat(combatants);
       CombatRunner.Run(combat, new ConsoleCombatLog(), new ConsoleCombatListener());
    }
 }
+
